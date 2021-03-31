86e84cfd57acc3e13c5d8273c466bdfcca8ee687
Caner Demir <cnerdmir@gmail.com>
2021-03-31 15:01:11 +0300

    Separate project components
    Separated project components into different projects for ease of use in development process.
    Created areas for the admin, lecturer and student part of the project and moved related files into related folders.

---
+++ OnlineExam.Utilities/SD.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineExam.Utilities
+{
+    public static class SD
+    {
+    }
+}
--- OnlineExam/Startup.cs
+++ OnlineExam/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OnlineExam.Data;
+using OnlineExam.DataAccessToDb.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -63,7 +63,7 @@ namespace OnlineExam
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Student}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
